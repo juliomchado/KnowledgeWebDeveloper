938a78ea8ebaf0258a474414eecb72ea9b9fa52b
juliomchado <juliocarlos00@hotmail.com>
2020-02-27 23:04:00 -0300

    Usando biblioteca e modulos de terceiros

---
+++ Codigos-Js/Node/usandoModulosTerceiros.js
@@ -0,0 +1,11 @@
+// Como usar modulos de terceiros
+
+const _ = require('lodash') //Ele procura automaticamente em node_modules
+setInterval(() => console.log(_.random(500, 600)), 2000) //.random = função do lodash
+
+
+
+
+
+
+
