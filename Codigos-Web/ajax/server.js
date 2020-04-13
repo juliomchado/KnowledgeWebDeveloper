990da73031d18733c35006cef56a73042004067c
juliomchado <juliocarlos00@hotmail.com>
2020-04-13 01:58:59 +0100

    Ajax, xmlhttprequest3, upload de arquivo, fetch api, axios

--- Codigos-Web/ajax/server.js
+++ Codigos-Web/ajax/server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const multer = require('multer')
 
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, './upload')
@@ -15,19 +19,30 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage }).single('arquivo')
 
-app.post('/upload', (req, res)=> {
+app.post('/upload', (req, res) => {
     upload(req, res, err => {
-        if(err){
+        if (err) {
             return res.end('Ocorreu um erro.')
         }
-        
+
         res.end('Concluído com sucesso.')
     })
 })
 
-app.use(express.static('.'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : impar
+    })
+})
+
 
 app.listen(8080, () => console.log('Executando servidor'))
 
