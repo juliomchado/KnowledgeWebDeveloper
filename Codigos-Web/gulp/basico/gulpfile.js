5b1fbb7bd0ab385306cd289dbedfc5666dfa828c
juliomchado <juliocarlos00@hotmail.com>
2020-05-12 19:49:09 +0100

    Entendendo gulp

--- Codigos-Web/gulp/basico/gulpfile.js
+++ Codigos-Web/gulp/basico/gulpfile.js
@@ -1,11 +1,36 @@
 const gulp = require('gulp')
-const { series } = require('gulp')
+const { series, parallel } = require('gulp')
 
-function copiar(cb) {
 
-    console.log('Tarefa de copiar!')
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
+    return cb()
+
+}
+
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!')
+    return cb()
+
+}
+
+const copiar = cb => {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+    // .pipe(imagePelaMetade()).pipe(ImagePretoEbanco())
     return cb()
 }
 
-module.exports.default = series(copiar)
+const fim = cb => {
+    console.log('Tarefa Fim')
+    return cb()
 
+}
+
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+)
