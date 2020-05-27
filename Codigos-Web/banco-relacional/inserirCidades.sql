ccc05348a15e518cfb4b6e021c4c699dfd8cd07d
juliomchado <juliocarlos00@hotmail.com>
2020-05-28 00:36:55 +0100

    cidades criadas

---
+++ Codigos-Web/banco-relacional/inserirCidades.sql
@@ -0,0 +1,35 @@
+select
+    *
+from
+    estados
+where
+    id = 29
+INSERT INTO
+    cidades(nome, area, estado_id)
+VALUES
+    ('Campinas', 795, 29)
+INSERT INTO
+    cidades (nome, area, estado_id) VALUE('Niterói', 133.9, 25)
+INSERT INTO
+    cidades (nome, area, estado_id)
+VALUES
+    (
+        'Caruaru',
+        920.6,
+        (
+            select
+                id
+            from
+                `estados`
+            where
+                sigla = 'PE'
+        )
+    )
+INSERT INTO
+    cidades (nome, area, estado_id)
+VALUES ('Juazeiro do Norte', 248.2, (select id from `estados` where sigla = 'CE'))
+
+
+select * from cidades
+
+delete from cidades where id = 5
