f5bb5d69782a5bdbf73ed8bdfeed0f9c87f4d5d1
juliomchado <juliocarlos00@hotmail.com>
2020-05-27 20:29:34 +0100

    Consultar com agrecacao (group by, sum(), avg()

---
+++ Codigos-Web/banco-relacional/consturarComAgregacao.sql
@@ -0,0 +1,29 @@
+-- sum = funcao de soma do SQL
+-- group by = agrupar por
+-- order by = ordernar por
+-- avg = media
+
+select
+    regiao as 'Região',
+    sum(populacao) as Total
+from
+    `estados`
+group by
+    regiao
+order by
+    Total desc
+select
+    sum(populacao) as Total
+from
+    `estados`
+order by
+    Total desc
+select
+    regiao as 'Região',
+    avg(populacao) as Total
+from
+    `estados`
+group by
+    regiao
+order by
+    Total desc
